bbbaf0c49ed298652294244f291f1e8c0d7ea02b
jayakrishnaa18 <110834392+jayakrishnaa18@users.noreply.github.com>
2025-05-18 18:27:15 +0530

    Update ForgotPassword.jsx

--- frontend/src/components/Auth/ForgotPassword.jsx
+++ frontend/src/components/Auth/ForgotPassword.jsx
@@ -13,7 +13,7 @@ const ForgotPassword = () => {
   const handleReset = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/forgot-password', {
+      const res = await axios.post('https://mern-task-0aws.onrender.com/api/auth/forgot-password', {
         email,
         securityAnswer,
         newPassword,
