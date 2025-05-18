c0eafaf2a112a479c26747af4ab67e9b3c64c6b1
jayakrishnaa18 <110834392+jayakrishnaa18@users.noreply.github.com>
2025-05-18 18:27:41 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('https://mern-task-0aws.onrender.com/api/auth/login', { email, password });
       localStorage.setItem('token', res.data.token);
 
       dispatch(loginSuccess({
